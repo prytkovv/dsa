a2a240d3d4f184f1fe6b37a6a04d90604e403e96
Andrew <dromadron@yandex.ru>
2022-02-11 02:53:20 +0300

    update quicksort

--- quicksort.py
+++ quicksort.py
@@ -1,9 +1,8 @@
 def quicksort(values, lo, hi):
-    if lo >= hi or lo < 0:
-        return
-    p = partition(values, lo, hi)
-    quicksort(values, lo, p)
-    quicksort(values, p + 1, hi)
+    if lo < hi:
+        p = partition(values, lo, hi)
+        quicksort(values, lo, p)
+        quicksort(values, p + 1, hi)
 
 
 def partition(values, lo, hi):
@@ -11,19 +10,17 @@ def partition(values, lo, hi):
     i = lo - 1
     j = hi + 1
     while True:
-        while True:
+        i += 1
+        while values[i] < pivot:
             i += 1
-            if values[i] >= pivot:
-                break
-        while True:
+        j -= 1
+        while values[j] > pivot:
             j -= 1
-            if values[j] <= pivot:
-                break
         if i >= j:
             return j
         values[i], values[j] = values[j], values[i]
 
 
 # import random
-# values = [random.randint(1, 100) for _ in range(10)]
+# values = [random.randint(1, 100) for _ in range(20)]
 # quicksort(values, 0, len(values) - 1)
