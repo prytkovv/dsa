ca7c2126b6fb546dfe288fe67ca8938c7c3cdaaf
Andrew <dromadron@yandex.ru>
2022-02-11 03:05:04 +0300

    add lcs algorithm

---
+++ lcs.py
@@ -0,0 +1,18 @@
+# Longest Common Subsequence
+# best explanation on the internet: https://www.youtube.com/watch?v=Ua0GhsJSlWM
+# wiki: https://en.wikipedia.org/wiki/Longest_common_subsequence_problem
+
+def lcs(s1, s2):
+    dp = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+    for i in reversed(range(len(s1))):
+        for j in reversed(range(len(s2))):
+            if s1[i] == s2[j]:
+                dp[i][j] = 1 + dp[i + 1][j + 1]
+            else:
+                dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+    return dp[0][0]
+
+
+# s1 = 'hieroglyphology'
+# s2 = 'michelangelo'
+# lcs(s1, s2)
