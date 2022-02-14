42ecdd51478ff9ed3071d0dc411749ed47ee937e
Andrew <dromadron@yandex.ru>
2022-02-15 02:06:44 +0300

    add coin change algoritm

---
+++ coin_change.py
@@ -0,0 +1,12 @@
+def change_coins(coins, amount):
+    dp = [float('inf') for _ in range(amount + 1)]
+    dp[0] = 0
+    for a in range(1, amount + 1):
+        for c in coins:
+            if a - c >= 0:
+                dp[a] = min(dp[a], dp[a - c] + 1)
+    return dp[amount]
+
+
+# coins => 2
+# change_coins([1, 3, 4, 5], 7)
